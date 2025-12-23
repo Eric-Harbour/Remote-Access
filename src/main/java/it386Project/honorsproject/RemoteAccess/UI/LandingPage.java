8fcac1854f290306e812cbe6a0c9144e89463b81
Eric-Harbour <harbour_eric@outlook.com>
2025-12-23 01:23:59 -0600

    created landingpage file to start splitting RMIWelcomeScreen

---
+++ src/main/java/it386Project/honorsproject/RemoteAccess/UI/LandingPage.java
@@ -0,0 +1,13 @@
+/**
+ * This will be the first page the user interacts with after the application opens.
+ * The user must be able to do the following:
+ *
+ * 1. If the user wants to connect to a remote system, they must be able to seelct the 'Connect to Remote System' button
+ * 2. If the user wants to allow for remote connections, they must be able to select the 'Allow Remote Connections' button
+ *
+ * **/
+
+package it386Project.honorsproject.RemoteAccess.UI;
+
+public class LandingPage {
+}
