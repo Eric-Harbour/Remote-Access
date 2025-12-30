9bba962f1fd10722e550e52c9340936103d64783
Eric-Harbour <harbour_eric@outlook.com>
2025-12-30 16:18:02 -0600

    created new client connect page and laid out what it should do

---
+++ src/main/java/it386Project/honorsproject/RemoteAccess/UI/ClientConnectPage.java
@@ -0,0 +1,23 @@
+/**
+ * This will be the resulting scene/page when a user presses the
+ * 'Connect to a Remote System' button.
+ *
+ * The user must be able to do the following:
+ *
+ * 1. At any step on this scene/page, allow the user to return
+ *      to the landing page by pressing the 'back' button on the top
+ *      left of the current page.
+ * 2. Enter the LOCAL IP ADDRESS of the system they want to connect to
+ * 3. Enter the LOCAL PORT NUMBER that the remote system opens
+ *
+ * **/
+
+package it386Project.honorsproject.RemoteAccess.UI;
+
+import javafx.scene.Scene;
+
+public class ClientConnectPage {
+//    public Scene loadClientConnectPage() {
+//
+//    }
+}
