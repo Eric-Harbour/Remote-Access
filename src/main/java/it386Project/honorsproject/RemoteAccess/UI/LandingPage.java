7dae5e0a12bdc2e6c70cf7daef374c68a33c32b6
Eric Harbour <eharbou@ilstu.edu>
2025-12-30 10:28:39 -0600

    created method to load landing page when application starts

--- src/main/java/it386Project/honorsproject/RemoteAccess/UI/LandingPage.java
+++ src/main/java/it386Project/honorsproject/RemoteAccess/UI/LandingPage.java
@@ -9,5 +9,58 @@
 
 package it386Project.honorsproject.RemoteAccess.UI;
 
-public class LandingPage {
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class LandingPage{
+    public Scene loadLandingPage() throws FileNotFoundException{
+        Image icon;
+        Scene landingPageScene;
+        try {
+            icon = new Image(new FileInputStream("src/resources/images/icon.png"));
+        } catch (FileNotFoundException e) {
+            icon = null;
+        }
+
+        // Create the panes we will use
+        BorderPane welcomePane = new BorderPane();
+
+        /**
+         * The client button and remote system button should
+         * probably be broken into their own individual sections
+         * Landing Page would be used to show the
+         * **/
+        // create items related to 'Connect to Remote System' (client) button
+        Pane buttonPane = new VBox(100);
+        Button clientButton = new Button("Connect to Remote System");
+        clientButton.setPrefHeight(50);
+        clientButton.setPrefWidth(200);
+
+        // create items related to 'Allow Remote Connection' (server)
+        Button serverButton = new Button("Allow Remote Connection");
+        serverButton.setPrefHeight(50);
+        serverButton.setPrefWidth(200);
+
+        // set the buttons into button pane
+        buttonPane.getChildren().addAll(clientButton, serverButton);
+
+        // set the buttonPane inside welcomePane
+        welcomePane.setCenter(buttonPane);
+
+        // set the welcomePane inside the scene
+        landingPageScene = new Scene(welcomePane);
+
+        return landingPageScene;
+    }
+    // The following variables will be used on multiple screens,
+
 }
